93bec60f01bbb9e4c96e601d64e38eb930413c2d
Nikhil <nikhilgupta.post@gmail.com>
2024-06-19 20:50:48 +0530

    add checkout page and subtotal

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,20 @@
 export const initialState = {
-    basket: [], 
+    basket: [
+        {
+            id:"2",
+            title:"Redmi 13C (Stardust Black, 6GB RAM, 128GB Storage) | Powered by 4G MediaTek Helio G85 | 90Hz Display | 50MP AI Triple Camera",
+            price:1476,
+            rating:2,
+            image:"https://m.media-amazon.com/images/I/41ykC0f0VTL.jpg"
+        },
+   
+], 
     use: null,
 };
+
+export const getBasketTotal = (basket)=>
+    basket?.reduce((amount, item)=>item.price + amount, 0);
+
 
 const reducer = (state, action)=>{
     console.log(action)
@@ -15,7 +28,26 @@ const reducer = (state, action)=>{
 
         case 'REMOVE_FROM_BASKET':
         //Logic for Removing item from basket
-        return { state };
+
+        //we cloned the basket
+        let newBasket = [...state.basket];
+
+        //we check to see if product exits,
+        const index = state.basket.findIndex((basketItem) => basketItem.id===action.id);
+
+        if(index>=0){
+            // item exists in baskt=et remove it...
+            newBasket.splice(index, 1);
+        }else{
+            console.warn(
+                `Cant remvoe product (id: ${action.id}) as its not exist`
+            )
+        }
+
+        return {
+             ...state, 
+             basket: newBasket 
+            };
 
         default:
             return state;
@@ -23,6 +55,3 @@ const reducer = (state, action)=>{
 }
 
 export default reducer;
-
-/*
-*/
